2f6a7ce07eb235b574e8e211dd3a3d0bb4bedcfc
Juan Beldi <juan.beldi@mediamonks.com>
2020-09-18 00:54:21 -0300

    remove default react-create-app unnecessary stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import {Button} from './components/Button/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <Button onClick={() => console.log('test')}>
           Test
         </Button>
