33135cd3f8b78869bd2bab70d48d45ac3f39b859
Juan Beldi <juan.beldi@mediamonks.com>
2020-09-22 02:11:38 -0300

    trigger generic modal open on button click

--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import { useAppCopies } from '../../context/appCopiesContext';
+import { useAppGenericModal } from '../../context/appGenericModalContext';
 import { Button } from '../Button/Button';
 
 type PageProps = {}
 
 export const Page: React.FC<PageProps> = () => {
     const { title, description, modalButton } = useAppCopies();
+    const setIsModalOpen = useAppGenericModal.useSetGenericModalData();
+
 
     return <>
         <header>
             <h1>{title}</h1>
             <h2>{description}</h2>
         </header>
-        <Button onClick={() => console.log(modalButton)}>
+        <Button onClick={() => setIsModalOpen(true)}>
             {modalButton}
         </Button>
     </>
