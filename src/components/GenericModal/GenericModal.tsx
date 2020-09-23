dc1e96ab138e3137034a6375ab022ff1dc494721
Juan Beldi <juan.beldi@mediamonks.com>
2020-09-22 22:32:17 -0300

    add basic modal animation with basic styles

--- src/components/GenericModal/GenericModal.tsx
+++ src/components/GenericModal/GenericModal.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { motion, AnimatePresence, useSpring } from 'framer-motion';
+import './GenericModal.scss';
 import { Button } from '../Button/Button';
 import { useAppGenericModal } from '../../context/appGenericModalContext';
 
@@ -18,14 +20,35 @@ export type GenericModalProps = {
 export const GenericModal: React.FC = () => {
     const genericModalContext = useAppGenericModal.useDataGenericModalData() as GenericModalProps;
 
-    if (!genericModalContext || !genericModalContext.isOpen) return null;
+    return (
+        <AnimatePresence>
+            {(genericModalContext && genericModalContext.isOpen) &&
+                <>
+                    <motion.div
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                        transition={{ duration: 0.25, ease: 'easeOut' }}
+                        className="generic-modal"
+                        onClick={genericModalContext.onClose}
+                    >
+                        <motion.div
+                            initial={{ y: '100vh' }}
+                            animate={{ y: '0' }}
+                            exit={{ y: '100vh' }}
+                            transition={{ type: 'spring', stiffness: 500, damping: 30 }}
+                            className="generic-modal-content"
+                            onClick={(event) => event.stopPropagation()}
+                        >
+                            <h3>{genericModalContext.copies.title}</h3>
+                            <p>{genericModalContext.copies.description}</p>
+                            <Button onClick={genericModalContext.onConfirm}>{genericModalContext.copies.confirm}</Button>
+                            <Button onClick={genericModalContext.onClose}>{genericModalContext.copies.cancel}</Button>
+                        </motion.div>
+                    </motion.div>
+                </>
+            }
+        </AnimatePresence>
+    );
 
-    const { copies, onConfirm, onClose } = genericModalContext;
-
-    return <div>
-        <h3>{copies.title}</h3>
-        <p>{copies.description}</p>
-        <Button onClick={onConfirm}>{copies.confirm}</Button>
-        <Button onClick={onClose}>{copies.cancel}</Button>
-    </div>;
 }
